205f9cf2c605dbd3e516d3a8b673f88c42e0fa70
veler86 <chlopak8612@wp.pl>
2017-11-09 05:56:20 +0000

    gulp file with the watch function, nprmc file, json and sass folder

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync').create();
+
+gulp.task("sass", function() {
+  return gulp.src('sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer({browsers: 'last 2 versions'})
+    ]))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server:
+    {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task('watch', ['sass', 'browserSync'], function(){
+  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('./*.html', browserSync.reload);
+  gulp.watch('js/**/*.js', browserSync.reload);
+});
