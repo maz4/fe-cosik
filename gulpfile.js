8f36bbd150885dcc260f63060202007b3e9c99d1
veler86 <chlopak8612@wp.pl>
2017-11-11 07:57:42 +0000

    fixed a path issue to normalize.scss

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,34 @@ const plumber = require('gulp-plumber');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
+const path = require('path');
 
 gulp.task("sass", function() {
   return gulp.src('sass/**/*.scss')
     .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      includePaths: path.join(__dirname, '/node_modules/normalize.scss/')
+    }).on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer({browsers: 'last 2 versions'})
+      autoprefixer({
+        browsers: 'last 2 versions'
+      })
     ]))
     .pipe(gulp.dest('./css'))
-    .pipe(browserSync.reload({ stream: true }));
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('browserSync', function(){
+gulp.task('browserSync', function() {
   browserSync.init({
-    server:
-    {
+    server: {
       baseDir: "./"
     }
   });
 });
 
-gulp.task('watch', ['sass', 'browserSync'], function(){
+gulp.task('watch', ['sass', 'browserSync'], function() {
   gulp.watch('sass/**/*.scss', ['sass']);
   gulp.watch('./*.html', browserSync.reload);
-  gulp.watch('js/**/*.js', browserSync.reload);
 });
