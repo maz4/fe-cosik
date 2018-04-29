42dacd230232b864606c3c00506fe943037f46fe
veler86 <chlopak8612@wp.pl>
2018-04-29 18:43:45 +0100

    refactor code

--- js/google-map.js
+++ js/google-map.js
@@ -1,20 +1,30 @@
-function initMap() {
+(function(window) {
   'use strict';
-  var position = {
-    lat: 34.8689738,
-    lng: -111.7700938
-  };
 
-  var map = new google.maps.Map(document.querySelector('.js-google-map'), {
-    zoom: 7,
-    center: position
-  });
-  var iconPicture = '../img/icon-map-marker.svg'
-  var marker = new google.maps.Marker({
-    position: position,
-    map: map,
-    icon: iconPicture,
-    title: 'Sedona'
-  });
-}
-google.maps.event.addDomListener(window, 'load', initMap);
+  window.sedona = window.sedona || {};
+
+  window.sedona.GoogleMaps = {
+    init: function(elem){
+      this.elem = elem;
+    },
+
+    initMap: function() {
+      var position = {
+        lat: 34.8689738,
+        lng: -111.7700938
+      };
+
+      var map = new google.maps.Map(this.elem, {
+        zoom: 7,
+        center: position
+      });
+      var iconPicture = '../img/icon-map-marker.svg'
+      var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        icon: iconPicture,
+        title: 'Sedona'
+      });
+    },
+  }
+}(window));
